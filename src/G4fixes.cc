4fb37a24b7f4bf35ddccec7dcfd262f4f513b883
Richard Jones <rjones30@gmail.com>
2015-07-18 11:32:04 -0400

    - Add python object for G4VisManager to enable access to this   class from python code.

--- src/G4fixes.cc
+++ src/G4fixes.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/python.hpp>
 
+#include <G4VisExecutive.hh>
+
 void G4fixes_init(int verbose=0)
 {
    if (verbose)
@@ -9,5 +11,14 @@ void G4fixes_init(int verbose=0)
 
 BOOST_PYTHON_MODULE(libG4fixes)
 {
+   using boost::python::class_;
+
    boost::python::def("G4fixes_init", &G4fixes_init);
+
+   class_<G4VisExecutive, G4VisExecutive*, boost::noncopyable>
+         ("G4VisExecutive",
+          "wrapper around G4VisManager that automatically detects and "
+          "registers visualization drivers supported by the G4 library build")
+      .def("Initialize", &G4VisExecutive::Initialize)
+   ;
 }
