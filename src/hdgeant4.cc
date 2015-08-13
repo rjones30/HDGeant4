6e7d20272f1f4ad1f4b309de083bd8e08b766046
Richard Jones <rjones30@gmail.com>
2015-08-13 13:44:57 -0400

    * src/hdgeant4.cc [rtj]    - Fix double-delete that happens if the G4SteppingVerbose object is      instantiated on the stack, because the Geant4 system thinks it owns      this object and deletes it on exit, whereas the compiler also tries      to reap it at the close of function scope. I now instantiate it on      the heap using "new", but without a matching "delete".

--- src/hdgeant4.cc
+++ src/hdgeant4.cc
@@ -42,8 +42,8 @@ int main(int argc,char** argv)
   }
 
   // User Verbose output class
-  GlueXSteppingVerbose verbosity;
-  G4VSteppingVerbose::SetInstance(&verbosity);
+  GlueXSteppingVerbose *verbosity = new GlueXSteppingVerbose();
+  G4VSteppingVerbose::SetInstance(verbosity);
   
   // Run manager handles the rest of the initialization
   G4RunManager runManager;
