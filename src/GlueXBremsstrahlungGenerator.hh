b7921e3f4015dd20ac303fb2665fa2900e6a4db5
Richard Jones <rjones30@gmail.com>
2017-02-05 22:25:10 -0500

    * src/GlueXBremsstrahlungGenerator.cc, src/GlueXBremsstrahlungGenerator.hh [rtj]    - add another bremsstrahlung generator, this one capable of resolving      photon polarization within restricted regions of the forward      bremsstrahlung cone.

---
+++ src/GlueXBremsstrahlungGenerator.hh
@@ -0,0 +1,53 @@
+//
+// GlueXBremsstrahlungGenerator class implementation
+//
+// author: richard.t.jones at uconn.edu
+// version: december 24, 2016
+//
+// In the context of the Geant4 event-level multithreading model,
+// this class is "shared", ie. has no thread-local state. It is
+// invoked from the GlueXGeneratorAction class, which is
+// responsible for managing the interlocks to ensure thread safety.
+//
+// Notes:
+// This is an auxilliary class that is not used in the main production
+// version of HDGeant4. It was written to provide an alternate means to
+// calculate statistical properties of the bremsstrahlung beam from the
+// underlying QED processes. The primary photon beam generator class is
+// GlueXPhotonBeamGenerator, which uses analytic distribution formulas
+// to generate distributions in photon energy, angle, and polarization.
+// By contrast, this GlueXBremsstrahlungGenerator directly computes the
+// QED amplitude for a specific choise for the event kinematics, and
+// then employs importance sampling to achieve a true reproduction of
+// the parent distribution without needing to know any formulas that
+// describe them. This latter method has the advantage that one can
+// use it to sample any property of the reaction (eg. the polarization
+// of the recoil electron, correlation of the photon polarization with
+// emission angle, etc.) simply by histogramming it over a sample.
+
+#ifndef GlueXBremsstrahlungGenerator_H
+#define GlueXBremsstrahlungGenerator_H
+
+#include <GlueXBremsstrahlungGenerator.hh>
+
+#if USING_DIRACXX
+
+GlueXBremsstrahlungGenerator::GlueXBremsstrahlungGenerator()
+ : fBeamEnergy(12.), fMinEnergy(3.)
+{}
+
+GlueXBremsstrahlungGenerator::~GlueXBremsstrahlungGenerator()
+{}
+
+void GlueXBremsstrahlungGenerator::GenerateBeamPhotons(int nevents)
+{}
+
+double GlueXBremsstrahlungGenerator::AtomicFormFactor(double q2)
+{}
+
+void GlueXBremsstrahlungGenerator::prepareImportanceSamplingPDFs()
+{}
+
+};
+
+#endif
