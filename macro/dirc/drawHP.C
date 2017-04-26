337051ee95cc0085bf918c8946e8e18ba60c2adc
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2017-04-26 20:25:37 +0200

    add macros for the DIRC

---
+++ macro/dirc/drawHP.C
@@ -0,0 +1,24 @@
+#define glx__sim
+#include "../../../sim-recon/src/plugins/Analysis/pid_dirc/DrcEvent.h"
+#include "glxtools.C"
+
+void drawHP(TString infile="drc.root"){
+  if(!glx_init(infile,1,"data/drawHP")) return;
+
+  DrcHit hit;
+  for (Int_t ievent=0; ievent<glx_ch->GetEntries(); ievent++){
+    glx_nextEvent(ievent,10);
+    
+    for(Int_t h=0; h<glx_event->GetHitSize(); h++){
+      hit = glx_event->GetHit(h);
+      Int_t mcp = hit.GetMcpId();
+      Int_t pix = hit.GetPixelId();
+      TVector3 gpos = hit.GetPosition();
+      Double_t time = hit.GetLeadTime();
+      if(mcp<102) glx_hdigi[mcp]->Fill(pix%8, 7-pix/8);
+    }
+  }
+  glx_drawDigi("m,p,v\n",0);
+  glx_canvasAdd(glx_cdigi);
+  // glx_canvasSave(1,0);
+}
