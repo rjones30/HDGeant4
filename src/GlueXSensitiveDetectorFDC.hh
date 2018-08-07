5f73c3c684b94e67a6f5ebba6d08c57d77be9881
Richard Jones <rjones30@gmail.com>
2018-08-07 18:50:34 -0400

    - include the missing include [rtj]

--- src/GlueXSensitiveDetectorFDC.hh
+++ src/GlueXSensitiveDetectorFDC.hh
@@ -71,10 +71,10 @@ class GlueXSensitiveDetectorFDC : public G4VSensitiveDetector
    static int    WIRES_PER_PLANE;
    static double WIRE_SPACING;
    static double STRIP_SPACING;
-   static double U_OF_WIRE_ZERO;
+   static double U_OF_WIRE_ONE;
    static int    STRIPS_PER_PLANE;
    static double CATHODE_ROT_ANGLE;
-   static double U_OF_STRIP_ZERO;
+   static double U_OF_STRIP_ONE;
    static double STRIP_GAP;
    static double K2;
    static double STRIP_NODES;
